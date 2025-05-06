4f8612e099435c6028b598131b45ff78fa09dbd3
Tim Herrin <teffluvium@gmail.com>
2025-05-06 11:18:41 -0700

    Adds from-iter and from_int methods to LIstNode class.
    * Updates tests.
    * Adds TRY003 to the [tool.ruff.lint.ignore] list.

--- src/leet_code/add_two_numbers.py
+++ src/leet_code/add_two_numbers.py
@@ -1,30 +1,6 @@
+from collections.abc import Iterable
 from dataclasses import dataclass
 from typing import Optional
-
-# def add_two_numbers(num_list_1: list[int], num_list_2: list[int]) -> list[int]:
-#     """
-#     Add two numbers represented by linked lists.
-
-#     :param num_list_1: First number as a linked list
-#     :param num_list_2: Second number as a linked list
-#     :return: Sum of the two numbers as a linked list
-#     """
-#     # Initialize the result list and carry
-#     result = []
-#     carry = 0
-
-#     # Iterate through both lists until both are empty and no carry is left
-#     while num_list_1 or num_list_2 or carry:
-#         # Get the values from the lists, defaulting to 0 if empty
-#         val1 = num_list_1.pop(0) if num_list_1 else 0
-#         val2 = num_list_2.pop(0) if num_list_2 else 0
-
-#         # Calculate the sum and carry
-#         total = val1 + val2 + carry
-#         carry = total // 10
-#         result.append(total % 10)
-
-#     return result
 
 
 # Definition for singly-linked list.
@@ -43,6 +19,57 @@ class ListNode:
             result.append(str(tmp.val))
             tmp = tmp.next
         return f"[{', '.join(result)}]"
+
+    # Define a method to initialize the ListNode with a Python list
+    @classmethod
+    def from_iter(cls, iterable: "Iterable[int]") -> "ListNode":
+        """Create a ListNode from an ordered iterable of integers."""
+
+        if not isinstance(iterable, Iterable):
+            raise TypeError("Input must be an iterable")
+
+        lst = list(iterable)
+        if not lst:
+            raise ValueError("Empty iterable")
+
+        # Validate that the iterable contains only integers between 0 and 9
+        if not all(isinstance(x, int) and 0 <= x <= 9 for x in lst):
+            raise ValueError("Iterable must contain integers between 0 and 9")
+
+        head = cls(lst[0])
+        current = head
+        for value in lst[1:]:
+            current.next = cls(value)
+            current = current.next
+        return head
+
+    @classmethod
+    def from_int(cls, number: int) -> "ListNode":
+        """Create a ListNode from an integer."""
+        if not isinstance(number, int):
+            raise TypeError("Input must be an integer")
+
+        if number < 0:
+            raise ValueError("Number must be non-negative")
+
+        digits = []
+        # Extract digits in reverse order
+        while number > 0:
+            digits.append(number % 10)
+            number //= 10
+            print(f"Extracted digit: {digits[-1]}, Remaining number: {number}")
+        if not digits:
+            digits.append(0)
+
+        # Create the linked list from the digits
+        head = cls(digits[0])
+        current = head
+        for digit in digits[1:]:
+            current.next = cls(digit)
+            current = current.next
+
+        # Return the head of the linked list
+        return head
 
 
 def add_two_numbers(
@@ -84,9 +111,25 @@ def add_two_numbers(
 
 if __name__ == "__main__":  # pragma: no cover
     # Example usage
-    list_1 = ListNode(2, ListNode(4, ListNode(3)))
-    list_2 = ListNode(5, ListNode(6, ListNode(4)))
+    # list_1 = ListNode(2, ListNode(4, ListNode(3)))
+    # list_2 = ListNode(5, ListNode(6, ListNode(4)))
+    list_1 = ListNode(0)
+    list_2 = ListNode(1)
     list_sum = add_two_numbers(list_1, list_2)
     print(f"l1     = {list_1}")
     print(f"l2     = {list_2}")
     print(f"result = {list_sum!s}")
+
+    print()
+    # Example usage of from_list method
+    list_3 = ListNode.from_iter([2, 4, 3])
+    list_4 = ListNode.from_iter([5, 6, 4])
+    list_sum_2 = add_two_numbers(list_3, list_4)
+    print(f"l1     = {list_3}")
+    print(f"l2     = {list_4}")
+    print(f"result = {list_sum_2!s}")
+
+    print()
+    # Example usage of from_int method
+    list_5 = ListNode.from_int(342)
+    print(f"list_5 = {list_5}")
--- tests/test_add_two_numbers.py
+++ tests/test_add_two_numbers.py
@@ -1,15 +1,122 @@
+from contextlib import nullcontext as does_not_raise
+
 import pytest
 
 from src.leet_code.add_two_numbers import ListNode, add_two_numbers
 
 
 @pytest.mark.parametrize(
+    "py_iter, expected, exception, message",
+    [
+        (
+            # Good case
+            [1, 2, 3],
+            ListNode(1, ListNode(2, ListNode(3))),
+            does_not_raise(),
+            None,
+        ),
+        (
+            # Non-iterable case
+            123,
+            None,
+            pytest.raises(TypeError),
+            "Input must be an iterable",
+        ),
+        (
+            # Empty iterable case
+            [],
+            None,
+            pytest.raises(ValueError),
+            "Empty iterable",
+        ),
+        (
+            # Negative integer case
+            [-1, 2, 3],
+            None,
+            pytest.raises(ValueError),
+            "Iterable must contain integers between 0 and 9",
+        ),
+    ],
+)
+def test_list_node_from_iter_with_exceptions(
+    py_iter: list,
+    expected: ListNode,
+    exception: type,
+    message: str,
+) -> None:
+    """
+    Test the from_iter method of ListNode with exceptions.
+
+    :param py_iter: Python iterable (e.g., list, tuple, ) used to create ListNode
+    :param expected: Expected ListNode
+    :param exception: Exception type to be raised
+    :param message: Message to be displayed if the test fails
+    """
+    with exception as exc_info:
+        result = ListNode.from_iter(py_iter)
+    assert message is None or str(exc_info.value) == message
+
+    # Check the result only if no exception is raised
+    if expected is not None:
+        assert str(expected) == str(result)
+
+
+@pytest.mark.parametrize(
+    "py_iter, expected, exception, message",
+    [
+        (
+            # Good case
+            1_007,
+            ListNode(7, ListNode(0, ListNode(0, ListNode(1)))),
+            does_not_raise(),
+            None,
+        ),
+        (
+            # Non-integer case
+            123.6,
+            None,
+            pytest.raises(TypeError),
+            "Input must be an integer",
+        ),
+        (
+            # Negative integer case
+            -123,
+            None,
+            pytest.raises(ValueError),
+            "Number must be non-negative",
+        ),
+    ],
+)
+def test_list_node_from_int_with_exceptions(
+    py_iter: list,
+    expected: ListNode,
+    exception: type,
+    message: str,
+) -> None:
+    """
+    Test the from_iter method of ListNode with exceptions.
+
+    :param py_iter: Python iterable (e.g., list, tuple, ) used to create ListNode
+    :param expected: Expected ListNode
+    :param exception: Exception type to be raised
+    :param message: Message to be displayed if the test fails
+    """
+    with exception as exc_info:
+        result = ListNode.from_int(py_iter)
+    assert message is None or str(exc_info.value) == message
+
+    # Check the result only if no exception is raised
+    if expected is not None:
+        assert str(expected) == str(result)
+
+
+@pytest.mark.parametrize(
     "l1, l2, expected",
     [
         (
-            ListNode(2, ListNode(4, ListNode(3))),
-            ListNode(5, ListNode(6, ListNode(4))),
-            ListNode(7, ListNode(0, ListNode(8))),
+            ListNode.from_iter([2, 4, 3]),
+            ListNode.from_iter([5, 6, 4]),
+            ListNode.from_iter([7, 0, 8]),
         ),
         (
             ListNode(0),
@@ -17,9 +124,9 @@ from src.leet_code.add_two_numbers import ListNode, add_two_numbers
             ListNode(0),
         ),
         (
-            ListNode(9, ListNode(9, ListNode(9))),
+            ListNode.from_iter([9, 9, 9]),
             ListNode(1),
-            ListNode(0, ListNode(0, ListNode(0, ListNode(1)))),
+            ListNode.from_iter([0, 0, 0, 1]),
         ),
     ],
 )
@@ -52,35 +159,6 @@ def test_add_two_numbers(l1: ListNode, l2: ListNode, expected: ListNode) -> None
         ),
     ],
 )
-def test_add_two_numbers_no_solution(l1: ListNode, l2: ListNode) -> None:
-    """
-    Test the add_two_numbers function with inputs that have no solution.
-
-    :param l1: First number as a linked list
-    :param l2: Second number as a linked list
-    """
-    result = add_two_numbers(l1, l2)
-    assert str(result) != str(l1), f"Expected not {l1}, but got {result}"
-    assert str(result) != str(l2), f"Expected not {l2}, but got {result}"
-
-
-@pytest.mark.parametrize(
-    "l1, l2",
-    [
-        (
-            ListNode(1, ListNode(2)),
-            ListNode(3, ListNode(4)),
-        ),
-        (
-            ListNode(0),
-            ListNode(1),
-        ),
-        (
-            ListNode(9),
-            ListNode(1),
-        ),
-    ],
-)
 def test_add_two_numbers_empty(l1: ListNode, l2: ListNode) -> None:
     """
     Test the add_two_numbers function with empty inputs.
@@ -93,80 +171,30 @@ def test_add_two_numbers_empty(l1: ListNode, l2: ListNode) -> None:
 
 
 @pytest.mark.parametrize(
-    "l1, l2",
+    "l1, l2, expected",
     [
         (
-            ListNode(1, ListNode(2)),
-            ListNode(3, ListNode(4)),
-        ),
-        (
-            ListNode(0),
-            ListNode(1),
-        ),
-        (
-            ListNode(9),
-            ListNode(1),
+            ListNode.from_iter([9, 9, 9, 9, 9, 9, 9]),
+            ListNode.from_iter([9, 9, 9, 9]),
+            ListNode.from_iter([8, 9, 9, 9, 0, 0, 0, 1]),
         ),
     ],
 )
-def test_add_two_numbers_large_numbers(l1: ListNode, l2: ListNode) -> None:
+def test_add_two_numbers_large_numbers(l1: ListNode, l2: ListNode, expected: ListNode) -> None:
     """
     Test the add_two_numbers function with large numbers.
 
     :param l1: First number as a linked list
     :param l2: Second number as a linked list
+    :param expected: Expected sum as a linked list
     """
     result = add_two_numbers(l1, l2)
     assert str(result) != str(l1), f"Expected not {l1}, but got {result}"
     assert str(result) != str(l2), f"Expected not {l2}, but got {result}"
+    assert str(result) == str(expected), f"Expected {expected}, but got {result}"
 
 
-@pytest.mark.parametrize(
-    "l1, l2",
-    [
-        (
-            ListNode(1, ListNode(2)),
-            ListNode(3, ListNode(4)),
-        ),
-        (
-            ListNode(0),
-            ListNode(1),
-        ),
-        (
-            ListNode(9),
-            ListNode(1),
-        ),
-    ],
-)
-def test_add_two_numbers_negative_numbers(l1: ListNode, l2: ListNode) -> None:
-    """
-    Test the add_two_numbers function with negative numbers.
-
-    :param l1: First number as a linked list
-    :param l2: Second number as a linked list
-    """
-    result = add_two_numbers(l1, l2)
-    assert str(result) != str(l1), f"Expected not {l1}, but got {result}"
-    assert str(result) != str(l2), f"Expected not {l2}, but got {result}"
-
-
-@pytest.mark.parametrize(
-    "l1, l2",
-    [
-        (
-            ListNode(1, ListNode(2)),
-            ListNode(3, ListNode(4)),
-        ),
-        (
-            ListNode(0),
-            ListNode(1),
-        ),
-        (
-            ListNode(9),
-            ListNode(1),
-        ),
-    ],
-)
+@pytest.mark.parametrize("l1, l2", [(ListNode(0), ListNode(0))])
 def test_add_two_numbers_zero(l1: ListNode, l2: ListNode) -> None:
     """
     Test the add_two_numbers function with zero.
@@ -176,4 +204,5 @@ def test_add_two_numbers_zero(l1: ListNode, l2: ListNode) -> None:
     """
     result = add_two_numbers(l1, l2)
     assert str(result) != "0", f"Expected not 0, but got {result}"
+    assert str(result) != "None", f"Expected not None, but got {result}"
     assert str(result) != "None", f"Expected not None, but got {result}"
